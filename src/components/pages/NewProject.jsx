c17301b87a09cd3734bfb30f798b5e86f15b3c04
Arthur Gehlen <1arthurgehlen@gmail.com>
2025-01-17 14:38:47 -0300

    Conexão com API.

--- src/components/pages/NewProject.jsx
+++ src/components/pages/NewProject.jsx
@@ -6,7 +6,7 @@ function NewProject() {
         <div className={styles.newproject_container}>
             <h1>Criar Projeto</h1>
             <p>Crie seu projeto para depois adicionar os serviços</p>
-            <p>formulario</p>
+            {/* <p>formulario</p> */}
             <ProjectForm btnText="Criar Projeto" />
         </div>
     )
