3464952f80ce432306bed8c70dcc779449a1e600
Arthur Gehlen <1arthurgehlen@gmail.com>
2025-01-17 16:18:31 -0300

    Inserindo dados no banco via API.

--- src/components/form/Select.jsx
+++ src/components/form/Select.jsx
@@ -4,13 +4,20 @@ function Select({ text, name, options, handleOnChange, value }) {
     return (
         <div className={styles.form_control}>
             <label htmlFor={name}>{text}</label>
-            <select name={name} id={name}>
+            <select
+                name={name}
+                id={value}
+                onChange={handleOnChange}
+                value={value || ''}>
+
                 <option>Selecione uma opção</option>
+
                 {options.map((option) => (
-                    <option id={option.id} key={option.id}>
-                        {option.name}   z
+                    <option value={option.id} key={option.id}>
+                        {option.name}
                     </option>
                 ))}
+
             </select>
         </div>
     )
