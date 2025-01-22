cb941d8ffa8f188e8ffe7fe9ddfe9762bebeef59
Arthur Gehlen <1arthurgehlen@gmail.com>
2025-01-22 00:33:39 -0300

    Removendo projetos.

--- src/components/pages/Projects.jsx
+++ src/components/pages/Projects.jsx
@@ -12,6 +12,7 @@ import styles from "./Projects.module.css"
 function Projects() {
     const [projects, setProjects] = useState([])
     const [removeLoading, setRemoveLoading] = useState(false)
+    const [projectMessage, setProjectMessage] = useState('')
 
     const location = useLocation()
     let message = ''
@@ -30,12 +31,26 @@ function Projects() {
                 .then(resp => resp.json())
                 .then(data => {
                     setProjects(data)
-                    console.log(data)
                     setRemoveLoading(true)
                 })
                 .catch((err) => console.log(err))
         }, 1000);
     }, [])
+
+    function removeProject(id) {
+        fetch(`http://localhost:5000/projects/${id}`, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        })
+            .then((resp) => resp.json)
+            .then(() => {
+                setProjects(projects.filter((project) => project.id !== id)) 
+                setProjectMessage('Projeto removido com sucesso!')
+            })
+            .catch((err) => console.log(err))
+    }
 
     return (
         <div className={styles.project_container}>
@@ -44,6 +59,7 @@ function Projects() {
                 <LinkButton to="/newproject" text="Criar Projeto"></LinkButton>
             </div>
             {message && <Message type="success" msg={message} />}
+            {projectMessage && <Message type="success" msg={projectMessage} />}
             <Container customClass="start">
                 {projects.length > 0 &&
                     projects.map((project) => (
@@ -53,6 +69,7 @@ function Projects() {
                             budget={project.budget}
                             category={project.category.name}
                             key={project.id}
+                            handleRemove={removeProject}
                         />
                     ))
                 }
